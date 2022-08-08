2799ef75a81f22fda525abf02f63b97afd244b3d
Victor Pinho <vtsppinho@gmail.com>
2022-08-08 14:11:54 -0300

    [20220808] Adicionado MapperStruct no projeto

---
+++ booking/src/main/java/br/com/booking/dto/BookingRequest.java
@@ -0,0 +1,13 @@
+package br.com.booking.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class BookingRequest {
+    private UUID identifier;
+    private String name;
+}
--- booking/src/main/java/br/com/booking/mapper/BookingMapper.java
+++ booking/src/main/java/br/com/booking/mapper/BookingMapper.java
@@ -2,16 +2,14 @@ package br.com.booking.mapper;
 
 
 import br.com.booking.domain.Booking;
+import br.com.booking.dto.BookingRequest;
 import br.com.booking.dto.BookingResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
-@Component
-public class BookingMapper {
-
-    public BookingResponse toResponse (Booking booking) {
-        return BookingResponse.builder()
-                .identifier(booking.getIdentifier())
-                .name(booking.getName())
-                .build();
-    }
+@Mapper (componentModel = "spring")
+public interface BookingMapper {
+    BookingResponse toResponse (Booking booking);
+    Booking toEntity (BookingRequest request);
 }
